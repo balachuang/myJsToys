6553e112e4266d284e44783f42e65c2501fa8755
balachuang <balachuang@gmail.com>
2021-04-22 22:32:52 +0800

    Add After Image

--- SwingLine/SwingLine.js
+++ SwingLine/SwingLine.js
@@ -5,6 +5,7 @@ let CONST_SWING_MAX_ANG = dia(10);
 let CONST_TIME_INTERVAL = 10;		// ms
 let CONST_SWING_CYCLE = 100;		// intreval count
 let CONST_SWING_DELAY = 5;		// interval count
+let CONST_AFTER_IMAGE_COUNT = 30;
 let CONST_SHOW_SECTION = false;		// interval count
 
 let headCircle = {x:0, y:0, radius:0};
@@ -55,12 +56,13 @@ window.onload = function()
 	$('#config-panel input').change(updateParameters);
 	$('#config-panel input').keyup(updateParameters);
 
+	prepareSvg();
 	intObj = setInterval(() => {
-		updateTextPath();
+		updateSwingLine();
 	}, CONST_TIME_INTERVAL);
 };
 
-function updateTextPath()
+function updateSwingLine()
 {
 	$('.swing-section-pnt').remove();
 
@@ -69,37 +71,41 @@ function updateTextPath()
 		y: headCircle.y + headCircle.radius * Math.sin(CONST_HEAD_ROTATE_COEF * timeClock / 2)
 	};
 
-	let newPath = 'M ' + xyPair(head);
-
-	for (var n=0; n<CONST_SECT_CNT; ++n)
+	for (var m=0; m<CONST_AFTER_IMAGE_COUNT; ++m)
 	{
-		let delay = n * CONST_SWING_DELAY;
-		let angle = CONST_SWING_MAX_ANG * Math.sin(Math.max(timeClock - delay, 0) * 2 * Math.PI / CONST_SWING_CYCLE);
-		sectPoint[n].x = head.x + (CONST_SECT_LEN * (n + 0.5)) * Math.cos(angle);
-		sectPoint[n].y = head.y + (CONST_SECT_LEN * (n + 0.5)) * Math.sin(angle);
+		let currTimeClock = timeClock - m * CONST_SWING_CYCLE / 100;
+		let newPath = 'M ' + xyPair(head);
 
-		// draw section
-		if (CONST_SHOW_SECTION)
+		for (var n=0; n<CONST_SECT_CNT; ++n)
 		{
-			var vo = makeSVG('circle', 	{cx:sectPoint[n].x, cy:sectPoint[n].y, r:5, class:'swing-section-pnt'});
-			$('#svg-area').append(vo);
+			let delay = n * CONST_SWING_DELAY;
+			let angle = CONST_SWING_MAX_ANG * Math.sin(Math.max(currTimeClock - delay, 0) * 2 * Math.PI / CONST_SWING_CYCLE);
+			sectPoint[n].x = head.x + (CONST_SECT_LEN * (n + 0.5)) * Math.cos(angle);
+			sectPoint[n].y = head.y + (CONST_SECT_LEN * (n + 0.5)) * Math.sin(angle);
+
+			// draw section
+			if (CONST_SHOW_SECTION && (m == 0))
+			{
+				var vo = makeSVG('circle', 	{cx:sectPoint[n].x, cy:sectPoint[n].y, r:5, class:'swing-section-pnt'});
+				$('#svg-area').append(vo);
+			}
 		}
-	}
 
-	let cent = {
-		x: (sectPoint[0].x + sectPoint[1].x) / 2 ,
-		y: (sectPoint[0].y + sectPoint[1].y) / 2
+		let cent = {
+			x: (sectPoint[0].x + sectPoint[1].x) / 2 ,
+			y: (sectPoint[0].y + sectPoint[1].y) / 2
+		}
+		newPath += ' Q ' + xyPair(sectPoint[0]) + ' ' + xyPair(cent);
+		for (var n=1; n<CONST_SECT_CNT-1; ++n)
+		{
+			cent.x = (sectPoint[n].x + sectPoint[n+1].x) / 2;
+			cent.y = (sectPoint[n].y + sectPoint[n+1].y) / 2;
+			newPath += ' Q ' + xyPair(sectPoint[n]) + ' ' + xyPair(cent);
+		}
+		newPath += ' Q ' + xyPair(sectPoint[CONST_SECT_CNT-1]) + ' ' + xyPair(sectPoint[CONST_SECT_CNT-1]);
+	
+		$('#swing-line-' + m).attr('d', newPath);
 	}
-	newPath += ' Q ' + xyPair(sectPoint[0]) + ' ' + xyPair(cent);
-	for (var n=1; n<CONST_SECT_CNT-1; ++n)
-	{
-		cent.x = (sectPoint[n].x + sectPoint[n+1].x) / 2;
-		cent.y = (sectPoint[n].y + sectPoint[n+1].y) / 2;
-		newPath += ' Q ' + xyPair(sectPoint[n]) + ' ' + xyPair(cent);
-	}
-	newPath += ' Q ' + xyPair(sectPoint[CONST_SECT_CNT-1]) + ' ' + xyPair(sectPoint[CONST_SECT_CNT-1]);
-
-	$('#timeT-text-path').attr('d', newPath);
 
 	timeClock += 1;
 }
@@ -127,12 +133,31 @@ function updateParameters()
 	CONST_TIME_INTERVAL = $('#const-time-interval').val();
 	CONST_SWING_CYCLE = $('#const-swing-cycle').val() / CONST_TIME_INTERVAL;
 	CONST_SWING_DELAY = $('#const-swing-delay').val() / CONST_TIME_INTERVAL;
+	CONST_AFTER_IMAGE_COUNT = $('#const-after-image-count').val();
 	CONST_SHOW_SECTION = $('#show-section').is(':checked');
 
 	clearInterval(intObj);
+	//timeClock = 0;
+	prepareSvg();
 	intObj = setInterval(() => {
-		updateTextPath();
+		updateSwingLine();
 	}, CONST_TIME_INTERVAL);
+}
+
+function prepareSvg()
+{
+	let darkest = 50;
+	let colorSeg = (255 - darkest) / CONST_AFTER_IMAGE_COUNT;
+
+	$('#svg-area').empty();
+
+	// create lines
+	for (let n=0; n<=CONST_AFTER_IMAGE_COUNT; ++n) {
+		let color = 255 - n * colorSeg;
+		let colorStr = 'rgb('+color+','+color+','+color+')';
+		var vo = makeSVG('path', {id:'swing-line-'+n, stroke:colorStr, fill:'none', d:''});
+		$('#svg-area').append(vo);
+	}
 }
 
 function makeSVG(tag, attrs) {
