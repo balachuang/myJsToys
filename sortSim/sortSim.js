6853be7dae38cbd596fecdebf66919e8ceed4b7a
balachuang <balachuang@gmail.com>
2021-06-05 09:57:36 +0800

    add Cocktail and Counting sort

--- sortSim/sortSim.js
+++ sortSim/sortSim.js
@@ -13,6 +13,7 @@ let activeIdxs = null;
 let data = null;
 
 let stepNo = 0;
+let startTime = 0;
 let forceStop = false;
 
 $(document).ready(initD3);
@@ -24,33 +25,57 @@ function initD3()
 
 	initData();
 	initBarChart();
+	showEstimation();
 
 	$('#reset').click(function(){
-		$('rect').remove();
-		$('#step-display-content').text('Click "Start" to sort...');
-		stepNo = 0;
+		reset();
+		// $('rect').remove();
+		// $('#step-display-content').text('Click "Start" to sort...');
+		// stepNo = 0;
 
-		DATA_COUNT = eval($('#data-count').val());
-		initData();
-		initBarChart();
+		// DATA_COUNT = eval($('#data-count').val());
+		// initData();
+		// initBarChart();
+		// showEstimation();
 	});
+	$('#reset-a').click(function(){ reset('asc'); });
+	$('#reset-d').click(function(){ reset('des'); });
 	$('#stop').click(function(){
 		forceStop = true;
 	});
 	$('#start').click(function(){
 		stepNo = 0;
 		forceStop = false;
+		startTime = new Date();
 		ANIMATE_INTERVAL = eval($('#delay-time').val());
 
 		let sortMethod = $('#sort-method').val();
 		sort(sortMethod);
 	});
+
+	$('#control-panel input').change(function(){ showEstimation(); });
+	$('#control-panel select').change(function(){ showEstimation(); });
 }
 
-function initData()
+function reset(sort)
+{
+	$('rect').remove();
+	$('#step-display-content').text('Click "Start" to sort...');
+	stepNo = 0;
+
+	DATA_COUNT = eval($('#data-count').val());
+	initData(sort);
+	initBarChart();
+	showEstimation();
+}
+
+function initData(sort)
 {
 	data = [];
 	for (var i=0; i<DATA_COUNT; ++i) data.push(10 + Math.round(990 * Math.random()));
+
+	if (sort == 'asc') data.sort(function(a, b){return a - b});
+	if (sort == 'des') data.sort(function(a, b){return b - a});
 
 	scaleX = d3.scale.linear().domain([0, DATA_COUNT * BAR_AREA]).range([0, $('#svg-area').width()]);
 	scaleY1 = d3.scale.linear().domain([0, d3.max(data)]).range([0, $('#svg-area').height()]); // use for Y-length
@@ -113,33 +138,128 @@ function sort(method)
 	case 'Bubble':
 		sort_Bubble();
 		break;
+	case 'Cocktail':
+		sort_Cocktail();
+		break;
 	case 'Quick':
 		sort_Quick(0, data.length-1);
+		break;
+	case 'Counting':
+		sort_Counting();
 		break;
 	}
 }
 
 async function sort_Bubble()
 {
-	for (var end=data.length-2; end>=0; --end)
+	let rgtSwapCnt = 1;
+	for (var end=data.length-2; end>=0; end -= rgtSwapCnt)
 	{
 		for (comp=0; comp<=end; ++comp)
 		{
-			swapByCompare(comp, comp+1);
+			let thisSwap = swapByCompare(comp, comp+1);
+			rgtSwapCnt = thisSwap ? 1 : ++rgtSwapCnt;
 
 			// update barchart and pause
-			activeIdxs = [comp, comp+1];
-			updateBarChart();
-			await sleep(ANIMATE_INTERVAL);
-
+			await updateAndBreak([comp, comp+1, end+1]);
 			if (forceStop) break;
 		}
 		if (forceStop) break;
 	}
 
-	activeIdxs = [];
-	updateBarChart()
-	showMessage('Sorted Complete.');
+	await updateAndBreak([]);
+	showFinish();
+}
+
+async function sort_Cocktail()
+{
+	let unsortedIdx = {lft: 0, rgt: data.length-1};
+	let swap = true;
+	while(swap)
+	{
+		swap = false;
+
+		// Head --> Tail
+		let rgtSwapCnt = 1;
+		for (var n=unsortedIdx.lft; n<unsortedIdx.rgt; ++n)
+		{
+			let thisSwap = swapByCompare(n, n+1);
+			rgtSwapCnt = thisSwap ? 1 : ++rgtSwapCnt;
+			swap = thisSwap || swap;
+
+			// update barchart and pause
+			await updateAndBreak([n, n+1, unsortedIdx.lft, unsortedIdx.rgt]);
+			if (forceStop) break;
+		}
+		unsortedIdx.rgt -= rgtSwapCnt;
+
+		// Tail --> Head
+		let lftSwapCnt = 1;
+		for (var n=unsortedIdx.rgt; n>unsortedIdx.lft; --n)
+		{
+			let thisSwap = swapByCompare(n-1, n);
+			lftSwapCnt = thisSwap ? 1 : ++lftSwapCnt;
+			swap = thisSwap || swap;
+
+			// update barchart and pause
+			await updateAndBreak([n-1, n, unsortedIdx.lft, unsortedIdx.rgt]);
+			if (forceStop) break;
+		}
+		unsortedIdx.lft += lftSwapCnt;
+
+		if (unsortedIdx.lft >= unsortedIdx.rgt) break;
+	}
+
+	await updateAndBreak([]);
+	showFinish();
+}
+
+async function sort_Counting()
+{
+	let coutingAry = new Array(1000);
+	coutingAry.fill(0);
+
+	let minValue = Number.MAX_SAFE_INTEGER;
+	let maxValue = Number.MIN_SAFE_INTEGER;
+
+	// counting
+	for (var n=0; n<data.length; ++n)
+	{
+		if (minValue > data[n]) minValue = data[n];
+		if (maxValue < data[n]) maxValue = data[n];
+
+		++coutingAry[data[n]];
+
+		showMessage('Count: ('+n+') ' + data[n]);
+		await updateAndBreak([n]);
+		if (forceStop) break;
+	}
+
+	// acumulate
+	showMessage('Accumlating...');
+	// let acc = coutingAry[minValue];
+	for (var n=minValue+1; n<=maxValue; ++n)
+	{
+		coutingAry[n] += coutingAry[n-1];
+
+		if (forceStop) break;
+	}
+
+	// re-fill
+	let tempData = Array.from(data);
+	for (var n=tempData.length-1; n>=0; --n)
+	{
+		let val = tempData[n];
+		let idx = --coutingAry[val];
+		data[idx] = val;
+
+		showMessage('Re-fill: ('+idx+') ' + data[idx]);
+		await updateAndBreak([idx]);
+		if (forceStop) break;
+	}
+
+	await updateAndBreak([]);
+	showFinish();
 }
 
 async function sort_Quick(lft, rgt)
@@ -159,9 +279,8 @@ async function sort_Quick(lft, rgt)
 
 	if ((lft == 0) && (rgt == data.length-1))
 	{
-		activeIdxs = [];
-		updateBarChart()
-		showMessage('Sorted Complete.');
+		await updateAndBreak([]);
+		showFinish();
 	}
 }
 
@@ -172,9 +291,7 @@ async function sort_QuickPartition(lft, rgt)
 	swap(lft, rgt);
 
 	// update barchart and pause
-	activeIdxs = [lft, rgt];
-	updateBarChart()
-	await sleep(ANIMATE_INTERVAL);
+	await updateAndBreak([lft, rgt]);
 
 	let storeIndex = lft;
 	for (var i=lft; i<rgt; ++i)
@@ -185,9 +302,7 @@ async function sort_QuickPartition(lft, rgt)
 			++storeIndex;
 
 			// update barchart and pause
-			activeIdxs = [storeIndex, i];
-			updateBarChart()
-			await sleep(ANIMATE_INTERVAL);
+			await updateAndBreak([lft, rgt, storeIndex, i]);
 		}
 		if (forceStop) break;
 	}
@@ -196,23 +311,34 @@ async function sort_QuickPartition(lft, rgt)
 	return storeIndex;
 }
 
-async function swapByCompare(idx1, idx2)
+function swapByCompare(idx1, idx2)
 {
-	if (idx1 == idx2) return;
+	if (idx1 == idx2) return false;
 
 	showMessage('Compare: ('+idx1+') ' + data[idx1] + ' : ('+idx2+') ' + data[idx2]);
-	if (data[idx1] > data[idx2]) swap(idx1, idx2);
-	else showMessage('No Swap.');
+	if (data[idx1] > data[idx2]) return swap(idx1, idx2);
+
+	// showMessage('No Swap.');
+	return false;
 }
 
-async function swap(idx1, idx2)
+function swap(idx1, idx2)
 {
-	if (idx1 == idx2) return;
+	if (idx1 == idx2) return false;
 
 	var temp = data[idx1];
 	data[idx1] = data[idx2];
 	data[idx2] = temp;
 	showMessage('Swap: ('+idx1+') ' + data[idx1] + ' : ('+idx2+') ' + data[idx2]);
+
+	return true;
+}
+
+async function updateAndBreak(activeBars)
+{
+	activeIdxs = activeBars;
+	updateBarChart();
+	await sleep(ANIMATE_INTERVAL);
 }
 
 function sleep(ms) {
@@ -221,9 +347,45 @@ function sleep(ms) {
 
 function showMessage(msg)
 {
-	var msssage = $('#step-display-content').html() + '<br>' + (++stepNo) + ': ' + msg;
-	$('#step-display-content').html(msssage);
+	$('#step-display-content').append('<br>' + (++stepNo) + ': ' + msg);
 	$('#step-display').scrollTop($('#step-display-content').height());
+}
+
+function showEstimation()
+{
+	DATA_COUNT = eval($('#data-count').val());
+	ANIMATE_INTERVAL = eval($('#delay-time').val());
+	let sortMethod = $('#sort-method').val();
+
+	let totalCount = 0;
+	let totalTime = 0;
+	switch(sortMethod)
+	{
+	case 'Bubble':
+		totalCount = DATA_COUNT * DATA_COUNT;
+		break;
+	case 'Cocktail':
+		totalCount = DATA_COUNT * DATA_COUNT;
+		break;
+	case 'Quick':
+		totalCount = Math.round(DATA_COUNT * Math.log2(DATA_COUNT));
+		break;
+	case 'Counting':
+		totalCount = DATA_COUNT * 2;
+		break;
+	}
+	totalTime = totalCount * ANIMATE_INTERVAL / 1000;
+	$('#estimation').text('Estimated need ' + totalCount + ' steps, ' + totalTime + ' seconds.');
+}
+
+function showFinish()
+{
+	showMessage('Sorted Complete.');
+
+	let endTime = new Date();
+	let totalTime = (endTime - startTime) / 1000;
+	$('#estimation').append('&nbsp;&nbsp;&nbsp;&nbsp;');
+	$('#estimation').append('Acture need ' + stepNo + ' steps, ' + totalTime + ' seconds.');
 }
 
 function useless()
