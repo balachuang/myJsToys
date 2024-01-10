5270b03c95d6bf743757fabdb246021d75079c28
balachuang <balachuang@gmail.com>
2024-01-10 21:39:51 +0800

    add Ocean image to My 3D Gallery

--- PhotoSwipe/My3DGallery/index.js
+++ PhotoSwipe/My3DGallery/index.js
@@ -1,9 +1,10 @@
 const imgItems = [
-	{ url: 'Images/Toilet.png',    w: 1920, h: 1080, desc: 'Toilet'     },
-	{ url: 'Images/SunnyRoom.png', w: 1600, h: 1200, desc: 'Sunny Room' },
-	{ url: 'Images/Mahjong.png',   w: 1920, h: 1080, desc: 'Mahjong'    },
-	{ url: 'Images/3Cups.png',     w: 1920, h: 1080, desc: 'Three Cups' },
-	{ url: 'Images/SnowMan.png',   w: 1920, h: 1080, desc: 'Snow Man'   }
+	{ url: 'Images/Ocean.png',     w: 1920, h: 1080, desc: 'Ocean Surface' },
+	{ url: 'Images/Toilet.png',    w: 1920, h: 1080, desc: 'Toilet'        },
+	{ url: 'Images/SunnyRoom.png', w: 1600, h: 1200, desc: 'Sunny Room'    },
+	{ url: 'Images/Mahjong.png',   w: 1920, h: 1080, desc: 'Mahjong'       },
+	{ url: 'Images/3Cups.png',     w: 1920, h: 1080, desc: 'Three Cups'    },
+	{ url: 'Images/SnowMan.png',   w: 1920, h: 1080, desc: 'Snow Man'      }
 ];
 
 const imgHtml = '<a href="{url}" data-pswp-width="{w}" data-pswp-height="{h}" target="_blank"> <img class="thumb-image" src="{url}" alt="{desc}" /></a>';
